c76a2d95b2eddfaf34779f36305a04dac98388c2
luqe010-sudo <luqe010-sudo@users.noreply.github.com>
2026-04-28 22:31:14 +0200

    fix: allowlist facebookexternalhit in robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,19 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Facebot',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://wypozycz.online/sitemap.xml',
   };
 }
